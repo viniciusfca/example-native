5239aa125fde9f574f433f3e7f38d95ba46eaa87
Vinícius Souza Oliveira <vinicius.soliveira@luizalabs.com>
2023-01-16 09:47:05 -0300

    Adicionado rota de healtcheck

--- src/main/java/com/examplenative/controller/Healthcheck.java
+++ src/main/java/com/examplenative/controller/Healthcheck.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Healthcheck {
 	
+	@GetMapping("healthcheck")
 	public ResponseEntity<?> healthCheck(){
 		var body = new HashMap<>();
 		body.put("application", "example-native");
